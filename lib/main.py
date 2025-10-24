b3352ee08229433d8e43c72016205f206b463a71
Marley Reeves <marley@marlstar.com>
2025-10-24 13:12:02 +1000

    refactor(modular): separate package uninstallation

--- lib/main.py
+++ lib/main.py
@@ -1,6 +1,7 @@
 import argparse
 from . import __version__, DEFAULT_REPOS, DEFAULT_TARGET
-from . import install_package, uninstall_package, show_package, list_installed
+from . import install_package, show_package
+from .uninstall import uninstall_package
 from .list import list_installed
 
 def main(argv: list[str]):
